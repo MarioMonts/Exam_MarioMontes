e336f546f7eb748bd41fe45b7d84b999b4a2a162
MarioMonts <mario.montes@uniat.edu.mx>
2023-03-23 10:56:30 -0600

    Switch

--- Exam_MarioMontes.cpp
+++ Exam_MarioMontes.cpp
@@ -1,11 +1,61 @@
-//Examen Mario Montes
-
+/// Examenes Mario Montes
 
 
 #include <iostream>
+#include <string.h>
+#include <cstdlib>
+
+
+
+using namespace std;
+
+int accion = 0;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+	cout << "Bienvenido a los Examens de Mario Montes\n";
+	cout << "Elige alguna de las siguientes opciones:\n \n";
+	cout << "1: Examen Parcial I\n";
+	cout << "2: Examen Parcial II\n";
+	cout << "3: Examen Parcial III\n";
+
+	cin >> accion;
+
+	switch (accion)
+	{
+
+	case 1:
+
+		cout << "Examen del Parcial I\n";
+
+		break;
+
+	case 2:
+
+		cout << "Examen del Parcial II\n";
+
+
+		break;
+
+	case 3:
+
+		cout << "Examen del Parcial III\n";
+
+		break;
+
+	default: cout << "Opcion No Valida\n";
+
+
+	}
+
+
+	cout << "Gracias por consultar los Examenes de Mario Montes\n";
+
+	return 0;
+
+
 }
 
